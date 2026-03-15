31e71e54aa958cebde10acac42e2624924832a55
Tony Ganchev <tony.ganchev@gmail.com>
2026-03-15 18:45:34 +0200

    chore: switch to @tony.ganchev/eslint-plugin-header
    Hi, team,
    
    I noticed you are using _eslint-plugin-header_ with the schema-turn-off
    workaround to support ESLint 9.
    
    I forked _@tony.ganchev/eslint-plugin-header_ mid-2024 to address this
    issue and hoped it would be a temporary measure but since the original
    has not been updated for five years I decided to continue improving the
    new plugin and have been doing so for the last two years.
    
    Specific improvements include:
    - full JSON schema for validating the configuration.
    - fixed multiple bugs with the behavior of the plugin on Windows.
    - many other bug-fixes.
    - improved autofixing and error-reporting behavior.
    - added support for leading pragma comments before the header such as
      `@jest-environment`.
    
    This fork is already widely used by other Microsoft projects especially
    in the VSCode plugin ecosystem (and I have sent a similar PR proposal
    to the core VSCode maintainers).
    
    Looking forward to your feedback.
    
    Signed-off-by: Tony Ganchev <tony.ganchev@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,7 @@
-import pluginHeader from "eslint-plugin-header";
+import pluginHeader from "@tony.ganchev/eslint-plugin-header";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 import validateToolNamesRule from "./eslint-rules/tool-name-lint-rule.js";
-
-pluginHeader.rules.header.meta.schema = false; // workaround for https://github.com/Stuk/eslint-plugin-header/issues/57
 
 export default tseslint.config(
   // Global ignores
